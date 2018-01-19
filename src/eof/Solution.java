315a2ab10e36f0faec4f9ce43e6b1aeedbacaceb
ar17 <archtector@gmail.com>
2018-01-19 22:01:41 +0200

    eof

---
+++ src/eof/Solution.java
@@ -0,0 +1,16 @@
+package eof;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        int i = 1;
+        while (str != null)
+        {
+            System.out.print(Integer.toString(i++) + ' ' + str + '\n');
+            str = scanner.nextLine();
+        }
+    }
+}
