ffb0cbdd2904436a19398ee42a381182ac9de325
ar17 <archtector@gmail.com>
2018-01-20 22:53:18 +0200

    left rotation

---
+++ src/leftrotation/Solution.java
@@ -0,0 +1,41 @@
+package leftrotation;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    static int[] leftRotation(int[] a, int d) {
+        int i;
+        for (i = 0; i < d; i++) {
+            leftRotatebyOne(a, a.length);
+        }
+        return a;
+    }
+
+    static void leftRotatebyOne(int arr[], int n)
+    {
+        int i, temp;
+        temp = arr[0];
+        for (i = 0; i < n - 1; i++)
+            arr[i] = arr[i + 1];
+        arr[i] = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int d = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i = 0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int[] result = leftRotation(a, d);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+
+
+        in.close();
+    }
+}
