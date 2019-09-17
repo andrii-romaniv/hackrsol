3752f1bf08bfd6dbcd1d35a6d8b2faa1ab7056dc
ar17 <archtector@gmail.com>
2019-09-17 20:52:35 +0300

    add selection sort

---
+++ src/algorithms/selection_sort/Solution.java
@@ -0,0 +1,18 @@
+package algorithms.selection_sort;
+
+public class Solution {
+    public static int[] selectionSortArray(int[] arr){
+        for (int i=0; i < arr.length-1; i++){
+            int small = i;
+            for (int j=i+1; j < arr.length; j++){
+                if (arr[j] < arr[small]){
+                    small = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[small];
+            arr[small] = temp;
+        }
+        return arr;
+    }
+}
