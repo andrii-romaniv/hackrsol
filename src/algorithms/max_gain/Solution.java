ed034956ff57def55aa4886a316ec5d5a4eb8706
ar17 <archtector@gmail.com>
2019-09-21 22:16:11 +0300

    fix maxgain

--- src/algorithms/max_gain/Solution.java
+++ src/algorithms/max_gain/Solution.java
@@ -4,13 +4,13 @@ public class Solution {
     public static int maxGain(int[] a) {
         int min = Integer.MAX_VALUE;
         int gain = 0;
-        for (int i=0; i < a.length; i++){
-            if (a[i] < min){
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < min) {
                 min = a[i];
             }
-            if (a[i] > min){
+            if (a[i] > min) {
                 int currGain = a[i] - min;
-                if (Math.abs(currGain) > Math.abs(gain)){
+                if (Math.abs(currGain) > Math.abs(gain)) {
                     gain = currGain;
                 }
             }
@@ -20,3 +20,4 @@ public class Solution {
 
 
     }
+}
