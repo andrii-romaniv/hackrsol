367b1f7c35f9344a21cea0476d5756a7fb562d7e
ar17 <archtector@gmail.com>
2019-09-22 23:31:41 +0300

    delete node at head in circluar linked list

---
+++ src/algorithms/delete_at_head_circular/Solution.java
@@ -0,0 +1,16 @@
+package algorithms.delete_at_head_circular;
+
+public class Solution {
+    public ListNode deleteAtHead(ListNode head) {
+        if (head == null) return null;
+        ListNode curr = head;
+
+        if (curr.next == curr) return null;
+
+        while (curr.next != head){
+            curr = curr.next;
+        }
+        curr.next = curr.next.next;
+        return head = curr.next;
+    }
+}
