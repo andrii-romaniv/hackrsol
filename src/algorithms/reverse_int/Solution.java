36c3f76c5afb66880c209519ce3bd513cc23fea8
ar17 <archtector@gmail.com>
2019-10-01 22:00:35 +0300

    reverse int

---
+++ src/algorithms/reverse_int/Solution.java
@@ -0,0 +1,24 @@
+package algorithms.reverse_int;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(reverseInt(-321));
+    }
+    public static int reverseInt(int x) {
+        boolean negative = false;
+        if (x < 0){
+            negative = true;
+            x *= -1;
+        }
+        int rev = 0;
+        while(x > 0)
+        {
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return negative ? -1*rev : rev;
+
+
+    }
+}
