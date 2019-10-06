4f3dedddbfd485ec0b9e8da04609dd840b072ed7
ar17 <archtector@gmail.com>
2019-10-06 21:19:51 +0300

    binary tree sum

---
+++ src/algorithms/binary_tree_sum_all/Solution.java
@@ -0,0 +1,10 @@
+package algorithms.binary_tree_sum_all;
+
+import algorithms.TreeNode;
+
+public class Solution {
+    public int sum(TreeNode root) {
+        if (root == null) return 0;
+        return root.data + sum(root.left) + sum(root.right);
+    }
+}
