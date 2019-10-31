08fbae3950aac01170872ac5764f8ade17ccfe10
ar17 <archtector@gmail.com>
2019-10-31 11:42:23 +0200

    remove unused import

--- src/algorithms/hackerrank/algorithms/valleys/Solution.java
+++ src/algorithms/hackerrank/algorithms/valleys/Solution.java
@@ -1,13 +1,7 @@
 package algorithms.hackerrank.algorithms.valleys;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
 public class Solution {
 
     // Complete the countingValleys function below.
