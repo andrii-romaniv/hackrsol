99c21954aee99a5926d549cd84e09e7bb8920755
ar17 <archtector@gmail.com>
2019-12-09 22:20:22 +0200

    reverse LinkedList

---
+++ src/algorithms/leetcode/linkedLists/reverseLinkedList/Solution.java
@@ -0,0 +1,20 @@
+package algorithms.leetcode.linkedLists.reverseLinkedList;
+
+import algorithms.firecode.ListNode;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null){
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+
+        }
+        return prev;
+
+    }
+}
