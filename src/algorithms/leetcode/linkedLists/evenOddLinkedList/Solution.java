9ed8857f89fad46eba4414cefbe7dbb06467344a
ar17 <archtector@gmail.com>
2019-12-15 18:09:17 +0200

    odEvenList

--- src/algorithms/leetcode/linkedLists/evenOddLinkedList/Solution.java
+++ src/algorithms/leetcode/linkedLists/evenOddLinkedList/Solution.java
@@ -4,16 +4,26 @@ import algorithms.firecode.ListNode;
 
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        ListNode p = head;
-        while (true){
-            if (p==null){
-                return p;
+        if (head == null) return null;
+        int index = 0;
+        ListNode curr = head;
+        ListNode evenNode = new ListNode(-1);
+        ListNode evenPointer = evenNode;
+        ListNode oddNode = new ListNode(-1);
+        ListNode oddPointer = oddNode;
+        while (curr != null){
+            if (index%2 == 0){
+                evenPointer.next = curr;
+                evenPointer = evenPointer.next;
+            } else {
+                oddPointer.next = curr;
+                oddPointer = oddPointer.next;
             }
-            if (p.next == null){
-                return p;
-            }
-            p = p.next.next;
+            curr = curr.next;
+            ++index;
         }
-
+        oddPointer.next = null;
+        evenPointer.next = oddNode.next;
+        return evenNode.next;
     }
 }
