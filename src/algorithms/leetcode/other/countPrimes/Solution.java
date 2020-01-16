cbd32ee042061e9ead5e2264c4f3970f798afe94
ar17 <archtector@gmail.com>
2020-01-16 21:13:04 +0200

    count primes

---
+++ src/algorithms/leetcode/other/countPrimes/Solution.java
@@ -0,0 +1,21 @@
+package algorithms.leetcode.other.countPrimes;
+
+class Solution {
+    public int countPrimes(int n) {
+        int count = 0;
+        boolean[] isPrime = new boolean[n];
+        for (int i =2; i < n; i++){
+            isPrime[i] = true;
+        }
+        for (int i = 2; i * i < n; i++){
+            if (!isPrime[i]) continue;
+            for(int j = i*i; j < n; j +=i){
+                isPrime[j] = false;
+            }
+        }
+        for(int i =2; i < n; i++){
+            if (isPrime[i]) count++;
+        }
+        return count;
+    }
+}
