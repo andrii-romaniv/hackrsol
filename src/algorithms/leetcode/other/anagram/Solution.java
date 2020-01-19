ed45d46f71fed2abe1c5131bda7cc9596c6ff7d0
ar17 <archtector@gmail.com>
2020-01-19 23:18:11 +0200

    check permutation palindrom only for small characters

--- src/algorithms/leetcode/other/anagram/Solution.java
+++ src/algorithms/leetcode/other/anagram/Solution.java
@@ -2,21 +2,40 @@ package algorithms.leetcode.other.anagram;
 
 class Solution {
     public static void main(String[] args) {
-        String s = "anagram";
-        String t = "nagaram";
-        System.out.println(isAnagram(s, t));
+        String a = "a";
+        String b = "aaa";
+        String c = "aab";
+        String d = "abc";
+        String e = "abba";
+        String f = "carerac";
+        String g = "nope";
+        String h = "nopen";
+        System.out.println(isAnagram(a));
+        System.out.println(isAnagram(b));
+        System.out.println(isAnagram(c));
+        System.out.println(isAnagram(d));
+        System.out.println(isAnagram(e));
+        System.out.println(isAnagram(f));
+        System.out.println(isAnagram(g));
+        System.out.println(isAnagram(h));
     }
-    public static boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
+    public static boolean isAnagram(String s) {
+        int mycount = 0;
         int[] count = new int[26];
         for (int i = 0; i < s.length(); i++){
             count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
         }
         for (int c : count){
-            if (c!=0) return false;
+            if (s.length() % 2 == 0){
+                if (c==1) mycount--;
+            } else {
+                if (c==1) mycount++;
+            }
         }
-        return true;
+        if (mycount == 0) return true;
+        if (s.length() % 2 == 0 && mycount < 0) return false;
+        if (s.length() % 2 != 0 && mycount == 1) return true;
+        return false;
 
     }
 }
