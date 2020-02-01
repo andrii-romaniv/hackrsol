48ad08a16a64616b6a566bd5c1cdb7592c115764
ar17 <archtector@gmail.com>
2020-02-01 22:21:19 +0200

    permutate palindrom any character

---
+++ src/algorithms/leetcode/other/permutatePalindromAnyChar/Solution.java
@@ -0,0 +1,15 @@
+package algorithms.leetcode.other.permutatePalindromAnyChar;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        Set<Character> chars = new HashSet<>();
+        for (char c : s.toCharArray()){
+            if (!chars.add(c))
+                chars.remove(c);
+        }
+        return chars.size() <=1;
+    }
+}
