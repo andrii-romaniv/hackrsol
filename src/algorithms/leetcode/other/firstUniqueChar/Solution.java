ce7175f0bb3726902674eaf4a2ba89249d987592
ar17 <archtector@gmail.com>
2020-02-18 22:42:40 +0200

    first unique char

---
+++ src/algorithms/leetcode/other/firstUniqueChar/Solution.java
@@ -0,0 +1,24 @@
+package algorithms.leetcode.other.firstUniqueChar;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static int firstUniqChar(String s) {
+        Map<Character, Integer> map =  new HashMap<>();
+        for (int i=0; i < s.length(); i++){
+            if (!map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), 1);
+            } else {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
