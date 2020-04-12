20c9b2d8cc8938cd32d44adbd25a85ad4ee402c5
ar17 <archtector@gmail.com>
2020-04-12 23:04:28 +0300

    missing number

---
+++ src/algorithms/leetcode/arrays/missingNumber/Solution.java
@@ -0,0 +1,11 @@
+package algorithms.leetcode.arrays.missingNumber;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int expectedSum = nums.length * (nums.length+1)/2;
+        int actualSum  = 0;
+        for (int i : nums) actualSum +=i;
+        return expectedSum-actualSum;
+
+    }
+}
