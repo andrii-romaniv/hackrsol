817bd72b3e82dd7274276f29f53137cbb76f49e1
ar17 <archtector@gmail.com>
2020-07-26 12:17:33 +0300

    extra candies - two loops

---
+++ src/algorithms/leetcode/kidsWithCandies/Solution.java
@@ -0,0 +1,29 @@
+package algorithms.leetcode.kidsWithCandies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> k = new ArrayList<>();
+        int max = 0;
+        for (int i : candies) {
+            if (i > max) {
+                max = i;
+            }
+        }
+
+        for (int i : candies){
+                if (max <= (i+extraCandies)){
+                    k.add(true);
+                } else {
+                    k.add(false);
+                }
+            }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        kidsWithCandies(new int[]{2,3,5,1,3}, 3);
+    }
+}
